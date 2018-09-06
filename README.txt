75859353343afdee9b7844fa84ebc3ff30387e8f
bap-ba-bip-boop <42996283+bap-ba-bip-boop@users.noreply.github.com>
2018-09-06 19:31:46 +0200

    further improvements

--- README.txt
+++ README.txt
@@ -9,3 +9,9 @@ also added an if statement that skips all calculations if n = k or k = 1
 (because there is only one way to place n items in 1 space or n items i n spaces)
 
 no changes in the number of calculations needed for a stirlingnumber.
+
+iteration 3:
+added a start and stop to the internal iterator so that only the stirlingnumbers needed for a calculation
+is calculated and they are only calculated once(they were only calculated once before but whatever).
+
+Next iteration will be to minimize the space needed even further.
--- main.cpp
+++ main.cpp
@@ -4,18 +4,21 @@
 int stirlingNumber(const int n, const int k) {
 	int returnValue = 1;
 	if ( k != 1 && k != n) {
-		int**matrix = new int*[2];
+		int**matrix = new int*[2], left = n - k;
 		bool c = 0;
 		for (int i = 0; i < 2; i++)
 			matrix[i] = new int[n];
 
-		int i = 0, j = 0;
-		for (i = 0; i < n; i++, c = !c)
-			for (j = 0; j <= i; j++)
+		int i = 0, j = 0, start = 0;
+		for (i = 0; i < n; i++, c = !c) {
+			if (i > left)
+				start++;
+			for (j = start; j <= i && j < k; j++)
 				if (j == 0 || j == i)
 					matrix[c][j] = 1;
 				else
 					matrix[c][j] = matrix[!c][j - 1] + (j + 1) * matrix[!c][j];
+		}
 		returnValue = matrix[!c][k - 1];
 
 		for (i = 0; i < 2; i++)
@@ -27,9 +30,9 @@ int stirlingNumber(const int n, const int k) {
 
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	int n = 6;
+	int n = 8, k = 6;
 
-	//stirlingNumber(n, 4);
+	//stirlingNumber(n, k);
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++)
 			std::cout << std::to_string(stirlingNumber(i, j)) << " ";
